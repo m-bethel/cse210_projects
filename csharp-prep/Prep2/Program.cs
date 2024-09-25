df50dddad8506a7b7fff9d162f87f198aa6e8f81
m-bethel <miles.d.bethel@gmail.com>
2024-09-24 20:51:06 -0600

    Completed Prep2 Activity

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+        Console.WriteLine("What was your final percentage? ");
+        string percentage = Console.ReadLine();
+
+        int x =int.Parse(percentage);
+
+        string letterGrade = "Letter";
+
+        if (x >= 90)
+        {
+            letterGrade="A";
+        }
+        else if (x >= 80 &&  x < 90)
+        {
+            letterGrade="B";
+        }
+        else if (x >= 70 && x < 80 )
+        {
+            letterGrade="C";
+        }
+        else if (x >= 60 && x < 70)
+        {
+            letterGrade="D";
+        }
+        else if (x < 60)
+        {
+            letterGrade="F";
+        }
+
+        Console.WriteLine($"Your final grade is: {letterGrade}");
+
+        if ( x >= 70)
+        {
+            Console.WriteLine("Congrats! You passed the class!");
+
+        }
+        else if (x < 70)
+        {
+            Console.WriteLine("Should have studied Harder! You failed the class :(");
+        }
     }
 }
