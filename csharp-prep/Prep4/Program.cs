b068449304962dc1592806464e78cdc6cad16e3d
m-bethel <miles.d.bethel@gmail.com>
2024-09-24 21:53:09 -0600

    Completed Prep4 Activity

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Please enter numbers to add to the list. Enter 0 to stop.");
+        List<int> numbers = new List<int>();
+
+
+        int x = -1;
+        int sum = 0;
+        int avg = 0;
+        int max = 0;
+
+        while (x!=0)
+        {
+            Console.Write("Enter a number: ");
+            string userInput = Console.ReadLine();
+            x = int.Parse(userInput);
+            if (x != 0)
+            {
+                numbers.Add(x);
+            }
+        }
+
+
+
+
+        foreach (int number in numbers)
+        {
+            sum = sum + number;
+            avg = sum/numbers.Count;
+            if (max < number)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {avg}");
+        Console.WriteLine($"The maximum v is: {max}");
+
+
     }
 }
