b21661dfe9a088d251a41dc19b252dac35b8c8db
m-bethel <miles.d.bethel@gmail.com>
2024-09-24 20:25:46 -0600

    Completed first prep activity

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first Name?");
+        string f_name = Console.ReadLine();
+
+        Console.WriteLine("What is your last Name?");
+        string l_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {l_name}, {f_name} {l_name}.");
+
     }
 }
