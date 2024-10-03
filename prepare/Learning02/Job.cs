672a1b967d65d948834bfb9616fb014684c91dec
m-bethel <miles.d.bethel@gmail.com>
2024-10-03 13:22:02 -0600

    working on week 3 activity

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -1,1 +1,12 @@
+public class Job
+{
+    public string _company  = "";
+    public string _jobtitle = "";
+    public int _startYear = 2024;
+    public int  _endYear = 2027;
 
+    public void Display()
+    {
+        Console.WriteLine($"{_jobtitle} ({_company}) {_startYear} - {_endYear}");
+    }
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Intel";
+        job1._jobtitle = "Principal Investigator";
+        job1._startYear = 2025;
+        job1._endYear = 2027;
+
+        
+
+        Job job2 = new Job();
+        job2._company = "INL";
+        job2._jobtitle= "Reactor Operator";
+        job2._startYear = 2028;
+        job2._endYear = 2035;
+
+        job1.Display();
     }
 }
