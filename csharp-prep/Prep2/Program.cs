5657060b65a631ff7ccf8d54968d7d628613817e
m-bethel <121979436+m-bethel@users.noreply.github.com>
2025-09-17 08:33:52 -0600

    completed basic prep2 assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your Grade Percentage?");
+        string gradeNumber = Console.ReadLine();
+
+        int grade = int.Parse(gradeNumber);
+
+        string letterGrade;
+
+        if (grade >= 90)
+        {
+            letterGrade = "A" ;
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B" ;
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C" ;
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D" ;
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letterGrade}");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Work harder dolt.");
+        }
     }
 }
