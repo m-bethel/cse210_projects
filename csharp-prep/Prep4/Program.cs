c289fd246f51b430adc1507f5c72d04bf38676b7
m-bethel <121979436+m-bethel@users.noreply.github.com>
2025-09-22 08:44:03 -0600

    completed prep4 assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter integers (type '0' to finish):");
+        while (true)
+        {
+            int input = int.Parse(Console.ReadLine());
+            if (input == 0)
+                break;
+            numbers.Add(input);
+        }
+        int sum = 0;
+        float average = 0;
+        int maximum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+            average = sum / numbers.Count;
+            maximum = numbers.Max();
+
+        }
+        Console.WriteLine($"Total: {sum}, Average: {average}, Largest: {maximum}");
     }
 }
