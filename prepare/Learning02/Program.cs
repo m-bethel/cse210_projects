e36b13d46524c0bc3929278da8d03bc3e30d8242
m-bethel <121979436+m-bethel@users.noreply.github.com>
2025-10-02 16:47:15 -0600

    completed learning activity 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Web Developer";
+        job2._company = "Google";
+        job2._startYear = 2023;
+        job2._endYear = 2025;
+
+        job1.Display();
+        job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "John Doe";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
     }
 }
+
+
+    
