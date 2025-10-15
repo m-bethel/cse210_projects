a52a4d2384da45ae5ac99819abc6e57e9b2a0230
m-bethel <121979436+m-bethel@users.noreply.github.com>
2025-10-15 08:21:33 -0600

    initial commit of learning  02, completed

---
+++ prepare/Learning03/Fraction.cs
@@ -0,0 +1,35 @@
+class Fraction
+{
+    private int _numerator;
+    private int _denominator;
+
+    public Fraction(int numerator, int denominator)
+    {
+        if (denominator == 0)
+        {
+            throw new ArgumentException("Denominator cannot be zero.");
+        }
+        _numerator = numerator;
+        _denominator = denominator;
+    }
+    public Fraction()
+    {
+        _numerator = 1;
+        _denominator = 1;
+    }
+    public Fraction(int numerator)
+    {
+        _numerator = numerator;
+        _denominator = 1;
+    }
+    public string GetFractionString()
+    {
+        return $"{_numerator}/{_denominator}";
+    }
+    public double GetDecimalValue()
+    {
+        return (double)_numerator / (double)_denominator;
+    
+    }
+
+}
